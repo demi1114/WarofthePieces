afc71a888b6f97c19c2c8dbfb897c46171cd4e0c
demi1114 <1et8491d215366@gmail.com>
2026-03-09 01:04:29 +0900

    【Abilityを仮実装完了/カードと駒に仮アタッチ完了】

--- War of the Pieces/Assets/Demi/Script/CardData.cs
+++ War of the Pieces/Assets/Demi/Script/CardData.cs
@@ -24,13 +24,6 @@ public class CardData : ScriptableObject
     public List<Ability> abilities = new List<Ability>();
 
     public CardCategory category;
-    public PieceAttribute targetPieceAttribute;
-    [Header("Effect Settings")]
-    public int amount = 1;        // 強化量・弱体量・ドロー枚数
-    public int targetCount = 1;   // 対象数（複数効果用）
-    [Header("Piece Add Settings")]
-    public PieceData specificPiece;          // 特定の駒
-    public List<PieceData> candidatePieces;  // ランダム候補リスト
 
     public virtual bool Resolve(int owner, Vector2Int targetPos)
     {
